b17b5ef9f891360b37cd5a0bdc3bba65fbfdbb3c
Illia Rahavoi <irahavoi@gmail.com>
2016-01-21 22:40:16 -0600

    Up

--- app/src/main/java/com/irahavoi/signindemo/MainActivity.java
+++ app/src/main/java/com/irahavoi/signindemo/MainActivity.java
@@ -4,13 +4,47 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
+
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
+    private Button signOutButton;
+    private Button revokeAccessButton;
+    private TextView signInStatusTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInButton = (SignInButton) findViewById(R.id.signInBtn);
+        signOutButton = (Button) findViewById(R.id.signOutBtn);
+        revokeAccessButton = (Button) findViewById(R.id.revokeAccessBtn);
+        signInStatusTextView = (TextView) findViewById(R.id.sign_in_status);
+
+        googleApiClient = buildGoogleApiClient();
+
+
+    }
+
+    private GoogleApiClient buildGoogleApiClient(){
+        return new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API, Plus.PlusOptions.builder().build())
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();
     }
 
     @Override
@@ -33,5 +67,44 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(!googleApiClient.isConnecting()){
+            //Handling clicks only when google api client is not transferring between connected and not connected
+            switch (v.getId()){
+                case R.id.signInBtn:
+                    signInStatusTextView.setText("Signing In..");
+                    resolveSignInError();
+                    break;
+                case R.id.signOutBtn:
+                    Plus.AccountApi.clearDefaultAccount(googleApiClient);
+                    googleApiClient.disconnect();
+                    googleApiClient.connect();
+                    break;
+                case R.id.revokeAccessBtn:
+                    Plus.AccountApi.clearDefaultAccount(googleApiClient);
+                    Plus.AccountApi.revokeAccessAndDisconnect(googleApiClient);
+                    googleApiClient = buildGoogleApiClient();
+                    googleApiClient.connect();
+
+            }
+        }
     }
 }
